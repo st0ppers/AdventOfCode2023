8853ff1d6fc7263a7ce9f5ad1c62dc6ce2b6d33b
Alexander Damov <alexsander.damov@yahoo.com>
2023-12-04 00:35:25 +0200

    First day

---
+++ DayOne/Program.cs
@@ -0,0 +1,20 @@
+﻿var sum = 0;
+var current = new List<int>();
+
+while (true)
+{
+  var input = Console.ReadLine();
+
+  foreach (char c in input)
+  {
+    if (char.IsDigit(c))
+    {
+      current.Add(int.Parse(c.ToString()));
+    }
+  }
+
+  sum = (current.First() * 10) + current.Last() + sum;
+  current.Clear();
+
+  Console.WriteLine(sum);
+}
