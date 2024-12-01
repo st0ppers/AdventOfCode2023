08aad120fb584f56cc51cb01723f13af02e60b9a
Alexander Damov <alexsander.damov@yahoo.com>
2024-12-01 14:58:57 +0200

    test 2

---
+++ 2023/csharp/DayOne/Program.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCode;
+
+var a = new Point() { x = 13,y = 15};
+var b = new Point() { x = 13, y = 15 };
+decimal asd = 0.1m;
+decimal sdf = 0.2m;
+Console.WriteLine(a == b);
+Console.WriteLine(asd + sdf);
+Console.WriteLine("\b");
+Console.WriteLine(String.Format("{0:0.0000000000000000000}", asd + sdf));
+var asaa = new Random();
+
+      // "123.46"
+
+Console.WriteLine(0.1 + 0.2);
+
+
+var sum = 0;
+var current = new List<int>();
+
+while (true)
+{
+  var input = Console.ReadLine();
+
+  foreach (char c in input)
+  {
+    if (char.IsDigit(c))
+    {
+      current.Add(int.Parse(c.ToString()));
+    }
+  }
+
+  sum = (current.First() * 10) + current.Last() + sum;
+  current.Clear();
+
+  Console.WriteLine(sum);
+}
