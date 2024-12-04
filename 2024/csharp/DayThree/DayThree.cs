9cde5495fcd521d24fa6d8741df2563a026481dd
Alexander Damov <alexsander.damov@yahoo.com>
2024-12-04 23:41:16 +0200

    Dat three part one only

---
+++ 2024/csharp/DayThree/DayThree.cs
@@ -0,0 +1,33 @@
+﻿using static DayOne.DayOne.DayOne;
+
+namespace DayOne.DayThree;
+
+public static class DayThree
+{
+    public static double Test()
+    {
+        var result = 0d;
+        var a = GetContentFromFile("./DayThree/DayThree.txt");
+        for (var i = 0; i < a.Length - 8; i++)
+        {
+            if (a[i] != 'm') continue;
+            if (a[i + 1] == 'u' && a[i + 2] == 'l' && a[i + 3] == '(')
+            {
+                var subs = a.Substring(i + 4);
+                for (int j = 0; j < subs.Length; j++)
+                {
+                    var containsComma = subs.Substring(0, j).Contains(',');
+                    var hasOnlyNumbers = subs.Substring(0, j).Split(",").Select(s => int.TryParse(s, out _)).All(b => b);
+                    if (subs[j] == ')' && j <= 7 && hasOnlyNumbers && containsComma)
+                    {
+                        var asd = subs.Substring(0, j).Split(',').Select(int.Parse).ToArray();
+                        result += asd[0] * asd[1];
+                        break;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
--- 2024/csharp/Program.cs
+++ 2024/csharp/Program.cs
@@ -1,8 +1,12 @@
 ﻿using static DayOne.DayOne.DayOne;
 using static DayOne.DayTwo.DayTwo;
+using static DayOne.DayThree.DayThree;
 
 // Console.WriteLine(GetAnswer());
 // Console.WriteLine(GetAnswerPart2());
 
-Console.WriteLine(PartOne());
-Console.WriteLine(PartTwo());
+// Console.WriteLine(PartOne());
+// Console.WriteLine(PartTwo());
+
+Console.WriteLine(Test());
+// Console.WriteLine(PartTwo());
